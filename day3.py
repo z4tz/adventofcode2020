d08f80273d6bc2b423fd6b3b6a02fa05c6b5e3df
jimmal <jimmy.malmqvist@maxiv.lu.se>
2020-12-03 14:48:55 +0100

    minor readability and typehints fixes

--- day3.py
+++ day3.py
@@ -3,18 +3,18 @@ from typing import List
 from functools import reduce
 
 
-def treeEncounters(trees: List[List[bool]], sidesteps: int = 3, straightsteps: int = 1):
+def treeEncounters(trees: List[List[bool]], sidesteps: int = 3, straightsteps: int = 1) -> int:
     width = len(trees[0])
-    pos = 0
+    column = 0
     count = 0
-    for i in range(0, len(trees), straightsteps):
-        if trees[i][pos]:
+    for row in range(0, len(trees), straightsteps):
+        if trees[row][column]:
             count += 1
-        pos = (pos + sidesteps) % width
+        column = (column + sidesteps) % width
     return count
 
 
-def minimizeEncounters(trees: List[List[bool]]):
+def minimizeEncounters(trees: List[List[bool]]) -> int:
     slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
     encounters = [treeEncounters(trees, *slope) for slope in slopes]
     return reduce(lambda x, y: x * y, encounters)
@@ -22,7 +22,7 @@ def minimizeEncounters(trees: List[List[bool]]):
 
 def main(day):
     data = aocinput(day)
-    data = [[True if char == '#' else False for char in line.strip()] for line in data]
+    data = [[char == '#' for char in line.strip()] for line in data]
     result1 = treeEncounters(data)
     result2 = minimizeEncounters(data)
     print(result1, result2)
